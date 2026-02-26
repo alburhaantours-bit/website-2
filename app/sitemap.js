059fc2fad49bd36c947da89c9c19996908993837
alburhaantours-bit <alburhaantours@gmail.com>
2026-02-26 14:07:09 -0500

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,1 +1,12 @@
+export default function sitemap() {
+  const baseUrl = "https://wnyrenovation.com";
 
+  return [
+    { url: `${baseUrl}/`, lastModified: new Date() },
+    { url: `${baseUrl}/services`, lastModified: new Date() },
+    { url: `${baseUrl}/gallery`, lastModified: new Date() },
+    { url: `${baseUrl}/about`, lastModified: new Date() },
+    { url: `${baseUrl}/contact`, lastModified: new Date() },
+    { url: `${baseUrl}/privacy`, lastModified: new Date() },
+  ];
+}
